de9e14abc3bfe5a65e355ac61a9ab55323951fd4
Jihwan Park <jhpark8904@gmail.com>
2018-05-17 18:01:46 +0900

    Add Button Controller

---
+++ Unity_RPG/Assets/Scripts/UI/ButtonControler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControler : MonoBehaviour {
+
+    //Main Menu Panel Button
+    [SerializeField]
+    private Button m_MainMenuBtn;
+
+
+    //Main List Panel Button
+    [SerializeField]
+    private Button m_MainListExitBtn;
+
+    // Use this for initialization
+    void Start() {
+
+        //Main menu button
+        if (m_MainMenuBtn)
+        {
+            m_MainMenuBtn.onClick.AddListener(MainMenuBtnOnClick);
+        }
+
+
+        //Main List Exit Button
+        if(m_MainListExitBtn)
+        {
+            m_MainListExitBtn.onClick.AddListener(MainListExitBtnOnClick);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void MainMenuBtnOnClick()
+    {
+        Debug.Log("Main Menu Clicked");
+    }
+
+    void MainListExitBtnOnClick()
+    {
+        Debug.Log("Main List Exit Clicked");
+    }
+
+}
---
+++ Unity_RPG/Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+    Button m_MenuButton;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        m_MenuButton = GetComponent<Button>();
+
+        //Attach Onclick Listener to the Button
+        m_MenuButton.onClick.AddListener(OnMenuBtnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMenuBtnClick()
+    {
+
+    }
+}
